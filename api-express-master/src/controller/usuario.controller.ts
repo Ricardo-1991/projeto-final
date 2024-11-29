da10f848c60e75b1d826f37b2b7e25fc939e3140
Ricardo <ricardo.dev1991@gmail.com>
2024-11-29 17:29:02 -0300

    feat: featorando código da camada controller e repository. Adicionando middleware de erros customizados e usando a biblioteca Zod para validações de entrada para a rota de usuários.

--- api-express-master/src/controller/usuario.controller.ts
+++ api-express-master/src/controller/usuario.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import usuarioRepository from "../repositories/usuario.repository";
+import { CustomError } from "../interfaces/customError";
 
 const findAll = async (req: Request, res: Response) => {
   const users = await usuarioRepository.findAll();
@@ -8,11 +9,10 @@ const findAll = async (req: Request, res: Response) => {
 
 const findById = async (req: Request, res: Response) => {
   const user = await usuarioRepository.findById(req.params.id);
-  if (user) {
-    res.json({ message: 'Usuário encontrado.', user });
-  } else {
-    res.status(404).json({ error: 'Usuário não encontrado.' });
+  if (!user) {
+    throw new CustomError("Usuário não encontrado.", 404);
   }
+  res.json({ message: 'Usuário encontrado.', user });
 };
 
 const createUser = async (req: Request, res: Response) => {
@@ -22,20 +22,18 @@ const createUser = async (req: Request, res: Response) => {
 
 const updateUser = async (req: Request, res: Response) => {
   const user = await usuarioRepository.update(req.params.id, req.body);
-  if (user) {
-    res.json({ message: 'Usuário atualizado.', user });
-  } else {
-    res.status(404).json({ error: 'Usuário não encontrado.' });
+  if (!user) {
+    throw new CustomError("Usuário não encontrado.", 404);
   }
+  res.json({ message: 'Usuário atualizado.', user });
 };
 
 const deleteUser = async (req: Request, res: Response) => {
   const user = await usuarioRepository.remove(req.params.id);
-  if (user) {
-    res.json({ message: 'Usuário deletado.', user });
-  } else {
-    res.status(404).json({ error: 'Usuário não encontrado.' });
+  if (!user) {
+    throw new CustomError("Usuário não encontrado.", 404);
   }
+  res.json({ message: 'Usuário deletado.', user });
 };
 
 export default { findAll, findById, createUser, updateUser, deleteUser };
