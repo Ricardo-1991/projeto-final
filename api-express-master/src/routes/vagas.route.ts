63e1e88f43f8c911a710d1fe534d5f3f64f57019
Ricardo <ricardo.dev1991@gmail.com>
2024-11-30 16:35:17 -0300

    feat: criado validação da rota de vagas.

--- api-express-master/src/routes/vagas.route.ts
+++ api-express-master/src/routes/vagas.route.ts
@@ -1,7 +1,20 @@
 import express from 'express';
 const router = express.Router();
+import {z} from 'zod';
 
 import vagasController from '../controller/vaga.controller';
+import {validator} from '../middlewares/validator'
+
+const statusEnum = z.enum(["Aberto", "Fechado"]);
+
+const jobSchema = z.object({
+    titulo: z.string().min(2, {message: "O título precisa ter pelo menos 2 caracteres"}),
+    descricao: z.string().min(10, {message: "O título precisa ter pelo menos 10 caracteres"}),
+    dataCadastro: z.string().min(10, {message: "A data precisa estar no formato dd/mm/aaaa"}),
+    telefone: z.string().min(11, {message: "O telefone precisa ter pelo menos 11 caracteres"}),
+    status: statusEnum,
+    empresa: z.string().min(2, {message: "O nome da empresa precisa ter pelo menos 2 caracteres"}),
+})
 
 // Retornas todas as vagas
 router.get('/', vagasController.findAll);
@@ -10,10 +23,10 @@ router.get('/', vagasController.findAll);
 router.get('/:id', vagasController.findById);
 
 // Cria uma nova vaga
-router.post('/', vagasController.create);
+router.post('/', validator(jobSchema) ,vagasController.create);
 
 // Faz Update de uma vaga
-router.put('/:id', vagasController.update);
+router.put('/:id', validator(jobSchema),vagasController.update);
 
 // Deleta uma vaga
 router.delete('/:id', vagasController.remove);
