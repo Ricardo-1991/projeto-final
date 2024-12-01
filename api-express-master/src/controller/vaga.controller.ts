09ac949866b196eabc089cf5949731159d6f6944
Ricardo <ricardo.dev1991@gmail.com>
2024-12-01 09:33:57 -0300

    fix: Refatorando tratamento de erros no controller da rota de Vagas.

--- api-express-master/src/controller/vaga.controller.ts
+++ api-express-master/src/controller/vaga.controller.ts
@@ -1,44 +1,63 @@
-import {Request, Response} from "express";
+import {Request, Response, NextFunction} from "express";
 import vagaService from "../services/vaga.service";
 import { CustomError } from "../interfaces/customError";
 
 
-const findAll = async (req: Request, res: Response) => {
-    const jobs = await vagaService.findAll();
-    res.json({ message: 'Listagem de vagas.', jobs });
-};
-
-const findById = async (req: Request, res: Response) => {
-    const params = req.params.id;
-    const job = await vagaService.findById(params);
-    if (!job) {
-        throw new CustomError("Vaga nao encontrada", 404);
+const findAll = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const jobs = await vagaService.findAll();
+        res.json({ message: 'Listagem de vagas.', jobs });
+    }catch(error){
+        next(error)
     }
-    res.status(200).json({ message: 'Vaga encontrada.', job });
 };
 
-const create = async (req: Request, res: Response) => {
-    const job = await vagaService.create(req.body);
-    res.status(201).json({ message: 'Vaga criada.', job });
-};
-
-const update = async (req: Request, res: Response) => {
-    const params = req.params.id;
-    const job = await vagaService.update(params, req.body);
-    if (!job) {
-        throw new CustomError("Vaga nao encontrada", 404);
+const findById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const params = req.params.id;
+        const job = await vagaService.findById(params);
+        if (!job) {
+            throw new CustomError("Vaga nao encontrada", 404);
+        }
+        res.status(200).json({ message: 'Vaga encontrada.', job })
+    }catch(error){
+        next(error)
     }
-    res.status(200).json({ message: 'Vaga atualizada.' });
 };
 
-const remove = async (req: Request, res: Response) => {
-    const params = req.params.id;
-    const job = await vagaService.remove(params);
-    if (!job) {
-        throw new CustomError("Vaga nao encontrada", 404);
+const create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const job = await vagaService.create(req.body);
+        res.status(201).json({ message: 'Vaga criada.', job });
+    }catch(error){
+        next(error)
     }
-    res.status(200).json({ message: 'Vaga deletada.', job });
-    
+};
+
+const update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const params = req.params.id;
+        const job = await vagaService.update(params, req.body);
+        if (!job) {
+            throw new CustomError("Vaga nao encontrada", 404);
+        }
+        res.status(200).json({ message: 'Vaga atualizada.' });
+    }catch(error){
+        next(error)
+    }
+};
+
+const remove = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const params = req.params.id;
+        const job = await vagaService.remove(params);
+        if (!job) {
+            throw new CustomError("Vaga nao encontrada", 404);
+        }
+        res.status(200).json({ message: 'Vaga deletada.', job });
+    }catch(error){
+        next(error)
+    }
 };
 
 
